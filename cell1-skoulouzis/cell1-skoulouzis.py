269c95ccbfdc149bf30bbd1f900826dbe3678381
skoulouzis <skoulouzis@users.noreply.github.com>
2022-09-29 14:26:18 +0200

    cell1-skoulouzis creation

---
+++ cell1-skoulouzis/cell1-skoulouzis.py
@@ -0,0 +1,24 @@
+
+import argparse
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+
+args = arg_parser.parse_args()
+
+id = args.id
+
+
+
+
+
+
+a = 1 + param_1
+
+import json
+filename = "/tmp/a_" + id + ".json"
+file_a = open(filename, "w")
+file_a.write(json.dumps(a))
+file_a.close()
