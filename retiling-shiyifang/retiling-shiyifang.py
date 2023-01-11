e558f86e44b2a22e2f93bdabc00c82b38cbea0f0
skoulouzis <skoulouzis@users.noreply.github.com>
2023-01-11 21:36:21 +0100

    retiling-shiyifang update

--- retiling-shiyifang/retiling-shiyifang.py
+++ retiling-shiyifang/retiling-shiyifang.py
@@ -1,5 +1,5 @@
-from laserfarm import Retiler
 import pathlib
+from laserfarm import Retiler
 
 import argparse
 arg_parser = argparse.ArgumentParser()
@@ -25,25 +25,25 @@ param_hostname = args.param_hostname
 param_login = args.param_login
 param_password = args.param_password
 
-conf_max_x = '398892.19'
-conf_remote_path_retiled = pathlib.Path( '/webdav/LAZ' + '/retiled_'+ 'myname')
-conf_max_y = '726783.87'
-conf_remote_path_split = pathlib.Path( '/webdav/LAZ' + '/split_'+ 'myname')
-conf_min_y = '214783.87'
-conf_min_x = '-113107.81'
 conf_n_tiles_side = '512'
-conf_local_tmp = pathlib.Path('/tmp')
+conf_min_x = '-113107.81'
+conf_remote_path_split = pathlib.Path( '/webdav/LAZ' + '/split_'+ 'Yifang')
+conf_min_y = '214783.87'
+conf_max_x = '398892.19'
+conf_remote_path_retiled = pathlib.Path( '/webdav/LAZ' + '/retiled_'+ 'Yifang')
 conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
+conf_local_tmp = pathlib.Path('/tmp')
+conf_max_y = '726783.87'
 
-conf_max_x = '398892.19'
-conf_remote_path_retiled = pathlib.Path( '/webdav/LAZ' + '/retiled_'+ 'myname')
-conf_max_y = '726783.87'
-conf_remote_path_split = pathlib.Path( '/webdav/LAZ' + '/split_'+ 'myname')
-conf_min_y = '214783.87'
-conf_min_x = '-113107.81'
 conf_n_tiles_side = '512'
-conf_local_tmp = pathlib.Path('/tmp')
+conf_min_x = '-113107.81'
+conf_remote_path_split = pathlib.Path( '/webdav/LAZ' + '/split_'+ 'Yifang')
+conf_min_y = '214783.87'
+conf_max_x = '398892.19'
+conf_remote_path_retiled = pathlib.Path( '/webdav/LAZ' + '/retiled_'+ 'Yifang')
 conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
+conf_local_tmp = pathlib.Path('/tmp')
+conf_max_y = '726783.87'
 
 grid_retile = {
     'min_x': float(conf_min_x),
