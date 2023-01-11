f74299e4ada4b7cc286be8afb6c32748a4cefab0
skoulouzis <skoulouzis@users.noreply.github.com>
2023-01-11 04:12:00 +0100

    feature-extraction-skoulouzis update

--- feature-extraction-skoulouzis/feature-extraction-skoulouzis.py
+++ feature-extraction-skoulouzis/feature-extraction-skoulouzis.py
@@ -12,7 +12,6 @@ arg_parser.add_argument('--tiles', action='store', type=str, required='True', de
 arg_parser.add_argument('--param_hostname', action='store', type=str, required='True', dest='param_hostname')
 arg_parser.add_argument('--param_login', action='store', type=str, required='True', dest='param_login')
 arg_parser.add_argument('--param_password', action='store', type=str, required='True', dest='param_password')
-arg_parser.add_argument('--param_username', action='store', type=str, required='True', dest='param_username')
 
 args = arg_parser.parse_args()
 print(args)
@@ -25,39 +24,38 @@ tiles = json.loads(args.tiles.replace('\'','').replace('[','["').replace(']','"]
 param_hostname = args.param_hostname
 param_login = args.param_login
 param_password = args.param_password
-param_username = args.param_username
 
-conf_min_x = '-113107.81'
-conf_attribute = 'raw_classification'
+conf_filter_type= 'select_equal'
+conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
+conf_tile_mesh_size = '10.'
 conf_local_tmp = pathlib.Path('/tmp')
-conf_apply_filter_value = '1'
+conf_attribute = 'raw_classification'
+conf_validate_precision = '0.001'
+conf_remote_path_retiled = pathlib.Path( '/webdav/LAZ' + '/retiled_'+ 'myname')
+conf_min_x = '-113107.81'
 conf_max_x = '398892.19'
 conf_min_y = '214783.87'
-conf_max_y = '726783.87'
-conf_tile_mesh_size = '10.'
-conf_validate_precision = '0.001'
-conf_remote_path_targets = pathlib.Path( '/webdav/LAZ' + '/targets_'+param_username)
-conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
-conf_remote_path_retiled = pathlib.Path( '/webdav/LAZ' + '/retiled_'+param_username)
-conf_filter_type= 'select_equal'
-conf_feature_name = 'perc_95_normalized_height'
 conf_n_tiles_side = '512'
-
-conf_min_x = '-113107.81'
-conf_attribute = 'raw_classification'
-conf_local_tmp = pathlib.Path('/tmp')
+conf_remote_path_targets = pathlib.Path( '/webdav/LAZ' + '/targets_'+ 'myname')
 conf_apply_filter_value = '1'
+conf_feature_name = 'perc_95_normalized_height'
+conf_max_y = '726783.87'
+
+conf_filter_type= 'select_equal'
+conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
+conf_tile_mesh_size = '10.'
+conf_local_tmp = pathlib.Path('/tmp')
+conf_attribute = 'raw_classification'
+conf_validate_precision = '0.001'
+conf_remote_path_retiled = pathlib.Path( '/webdav/LAZ' + '/retiled_'+ 'myname')
+conf_min_x = '-113107.81'
 conf_max_x = '398892.19'
 conf_min_y = '214783.87'
-conf_max_y = '726783.87'
-conf_tile_mesh_size = '10.'
-conf_validate_precision = '0.001'
-conf_remote_path_targets = pathlib.Path( '/webdav/LAZ' + '/targets_'+param_username)
-conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
-conf_remote_path_retiled = pathlib.Path( '/webdav/LAZ' + '/retiled_'+param_username)
-conf_filter_type= 'select_equal'
-conf_feature_name = 'perc_95_normalized_height'
 conf_n_tiles_side = '512'
+conf_remote_path_targets = pathlib.Path( '/webdav/LAZ' + '/targets_'+ 'myname')
+conf_apply_filter_value = '1'
+conf_feature_name = 'perc_95_normalized_height'
+conf_max_y = '726783.87'
     
 for t in tiles:
     features = [conf_feature_name]
