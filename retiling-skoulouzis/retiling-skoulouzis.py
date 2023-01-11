423dbceb38d8ae4271a5827bb53641a7dd3c4481
skoulouzis <skoulouzis@users.noreply.github.com>
2023-01-11 04:11:15 +0100

    retiling-skoulouzis update

--- retiling-skoulouzis/retiling-skoulouzis.py
+++ retiling-skoulouzis/retiling-skoulouzis.py
@@ -25,25 +25,25 @@ param_hostname = args.param_hostname
 param_login = args.param_login
 param_password = args.param_password
 
-conf_min_y = '214783.87'
-conf_local_tmp = pathlib.Path('/tmp')
-conf_min_x = '-113107.81'
-conf_n_tiles_side = '512'
-conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
-conf_max_y = '726783.87'
-conf_remote_path_retiled = pathlib.Path( '/webdav/LAZ' + '/retiled_'+ 'myname')
-conf_max_x = '398892.19'
 conf_remote_path_split = pathlib.Path( '/webdav/LAZ' + '/split_'+ 'myname')
+conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
+conf_local_tmp = pathlib.Path('/tmp')
+conf_remote_path_retiled = pathlib.Path( '/webdav/LAZ' + '/retiled_'+ 'myname')
+conf_min_x = '-113107.81'
+conf_max_x = '398892.19'
+conf_min_y = '214783.87'
+conf_n_tiles_side = '512'
+conf_max_y = '726783.87'
 
-conf_min_y = '214783.87'
-conf_local_tmp = pathlib.Path('/tmp')
-conf_min_x = '-113107.81'
-conf_n_tiles_side = '512'
-conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
-conf_max_y = '726783.87'
-conf_remote_path_retiled = pathlib.Path( '/webdav/LAZ' + '/retiled_'+ 'myname')
-conf_max_x = '398892.19'
 conf_remote_path_split = pathlib.Path( '/webdav/LAZ' + '/split_'+ 'myname')
+conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
+conf_local_tmp = pathlib.Path('/tmp')
+conf_remote_path_retiled = pathlib.Path( '/webdav/LAZ' + '/retiled_'+ 'myname')
+conf_min_x = '-113107.81'
+conf_max_x = '398892.19'
+conf_min_y = '214783.87'
+conf_n_tiles_side = '512'
+conf_max_y = '726783.87'
 
 grid_retile = {
     'min_x': float(conf_min_x),
