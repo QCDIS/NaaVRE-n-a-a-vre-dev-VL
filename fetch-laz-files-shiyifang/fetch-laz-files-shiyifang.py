a30538a518643c217fae76c1c6537f15025679b7
skoulouzis <skoulouzis@users.noreply.github.com>
2023-01-11 11:04:09 +0100

    fetch-laz-files-shiyifang update

--- fetch-laz-files-shiyifang/fetch-laz-files-shiyifang.py
+++ fetch-laz-files-shiyifang/fetch-laz-files-shiyifang.py
@@ -1,6 +1,6 @@
-from laserfarm.remote_utils import list_remote
 import pathlib
 from laserfarm.remote_utils import get_wdclient
+from laserfarm.remote_utils import list_remote
 
 import argparse
 arg_parser = argparse.ArgumentParser()
@@ -11,9 +11,9 @@ arg_parser.add_argument('--id', action='store', type=str, required=True, dest='i
 arg_parser.add_argument('--param_hostname', action='store', type=str, required='True', dest='param_hostname')
 arg_parser.add_argument('--param_login', action='store', type=str, required='True', dest='param_login')
 arg_parser.add_argument('--param_password', action='store', type=str, required='True', dest='param_password')
-arg_parser.add_argument('--param_remote_path_ahn', action='store', type=str, required='True', dest='param_remote_path_ahn')
 
 args = arg_parser.parse_args()
+print(args)
 
 id = args.id
 
@@ -21,12 +21,12 @@ id = args.id
 param_hostname = args.param_hostname
 param_login = args.param_login
 param_password = args.param_password
-param_remote_path_ahn = args.param_remote_path_ahn
 
-conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
+conf_remote_path_ahn =  '/webdav/LAZ'
 
+conf_remote_path_ahn =  '/webdav/LAZ'
 conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
-laz_files = [f for f in list_remote(get_wdclient(conf_wd_opts), pathlib.Path(param_remote_path_ahn).as_posix())
+laz_files = [f for f in list_remote(get_wdclient(conf_wd_opts), pathlib.Path(conf_remote_path_ahn).as_posix())
              if f.lower().endswith('.laz')]
 
 import json
