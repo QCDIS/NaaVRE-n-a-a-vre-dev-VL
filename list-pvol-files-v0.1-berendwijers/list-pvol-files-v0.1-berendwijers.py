b1b665c6117a45846eae366d1d5c51a09c6aa3f4
skoulouzis <skoulouzis@users.noreply.github.com>
2023-01-12 15:50:27 +0100

    list-pvol-files-v0.1-berendwijers update

--- list-pvol-files-v0.1-berendwijers/list-pvol-files-v0.1-berendwijers.py
+++ list-pvol-files-v0.1-berendwijers/list-pvol-files-v0.1-berendwijers.py
@@ -8,14 +8,13 @@ arg_parser.add_argument('--id', action='store', type=str, required=True, dest='i
 
 arg_parser.add_argument('--param_radar', action='store', type=str, required='True', dest='param_radar')
 
-
 args = arg_parser.parse_args()
 print(args)
 
 id = args.id
 
-param_radar = args.param_radar
 
+param_radar = args.param_radar
 
 
 conf_root_dir = Path('./data/PVOL/')
@@ -25,6 +24,7 @@ radar = radar_translation.get(param_radar)
 radar_pvol_dir = conf_root_dir.joinpath(radar)
 
 pvol_paths = radar_pvol_dir.glob("**/*.h5")
+
 
 import json
 filename = "/tmp/pvol_paths_" + id + ".json"
