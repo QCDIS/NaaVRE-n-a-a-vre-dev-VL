95504d35a4304bf1639db37c22dabeb4d745a2a2
skoulouzis <skoulouzis@users.noreply.github.com>
2023-01-12 12:49:58 +0100

    retrive-normed-tiles-shiyifang creation

---
+++ retrive-normed-tiles-shiyifang/retrive-normed-tiles-shiyifang.py
@@ -0,0 +1,42 @@
+import fnmatch
+import pathlib
+from laserfarm.remote_utils import list_remote
+from laserfarm.remote_utils import get_wdclient
+
+import argparse
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--remote_path_norm', action='store', type=str, required='True', dest='remote_path_norm')
+
+arg_parser.add_argument('--param_hostname', action='store', type=str, required='True', dest='param_hostname')
+arg_parser.add_argument('--param_login', action='store', type=str, required='True', dest='param_login')
+arg_parser.add_argument('--param_password', action='store', type=str, required='True', dest='param_password')
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+remote_path_norm = args.remote_path_norm
+
+param_hostname = args.param_hostname
+param_login = args.param_login
+param_password = args.param_password
+
+conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
+conf_remote_path_norm = pathlib.Path( '/webdav/LAZ' + '/norm_'+ 'YF')
+
+conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
+conf_remote_path_norm = pathlib.Path( '/webdav/LAZ' + '/norm_'+ 'YF')
+remote_path_norm
+tiles = [t.strip('/') for t in list_remote(get_wdclient(conf_wd_opts), conf_remote_path_norm.as_posix())
+         if fnmatch.fnmatch(t, 'tile_*_*.laz')]
+
+import json
+filename = "/tmp/tiles_" + id + ".json"
+file_tiles = open(filename, "w")
+file_tiles.write(json.dumps(tiles))
+file_tiles.close()
