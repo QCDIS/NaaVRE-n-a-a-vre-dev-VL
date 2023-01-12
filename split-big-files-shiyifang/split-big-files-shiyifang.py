ba0235c11ef62f9c646fbaca768ba835f7cb37d3
skoulouzis <skoulouzis@users.noreply.github.com>
2023-01-12 15:22:15 +0100

    split-big-files-shiyifang update

--- split-big-files-shiyifang/split-big-files-shiyifang.py
+++ split-big-files-shiyifang/split-big-files-shiyifang.py
@@ -1,8 +1,8 @@
-from webdav3.client import Client
-import laspy
-import pathlib
-import os
 import numpy as np
+from webdav3.client import Client
+import os
+import pathlib
+import laspy
 
 import argparse
 arg_parser = argparse.ArgumentParser()
@@ -29,18 +29,18 @@ param_login = args.param_login
 param_password = args.param_password
 
 conf_remote_path_split = pathlib.Path( '/webdav/LAZ' + '/split_'+ 'Yifang')
-conf_max_filesize = '262144000'  # desired max file size (in bytes)
+conf_remote_path_ahn =  '/webdav/LAZ'
 conf_remote_path_retiled = pathlib.Path( '/webdav/LAZ' + '/retiled_'+ 'Yifang')
+conf_max_filesize = '262144000'  # desired max file size (in bytes)
 conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
 conf_laz_compression_factor = '7'
-conf_remote_path_ahn =  '/webdav/LAZ'
 
 conf_remote_path_split = pathlib.Path( '/webdav/LAZ' + '/split_'+ 'Yifang')
-conf_max_filesize = '262144000'  # desired max file size (in bytes)
+conf_remote_path_ahn =  '/webdav/LAZ'
 conf_remote_path_retiled = pathlib.Path( '/webdav/LAZ' + '/retiled_'+ 'Yifang')
+conf_max_filesize = '262144000'  # desired max file size (in bytes)
 conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
 conf_laz_compression_factor = '7'
-conf_remote_path_ahn =  '/webdav/LAZ'
 
 def save_chunk_to_laz_file(in_filename, 
                            out_filename, 
