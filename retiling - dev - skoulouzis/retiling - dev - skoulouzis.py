4fec55d0c9f4b11d737e5297ea2a451291313236
skoulouzis <skoulouzis@users.noreply.github.com>
2023-01-16 22:20:25 +0100

    retiling - dev - skoulouzis update

--- retiling - dev - skoulouzis/retiling - dev - skoulouzis.py
+++ retiling - dev - skoulouzis/retiling - dev - skoulouzis.py
@@ -19,13 +19,10 @@ split_laz_files = json.loads(args.split_laz_files.replace('\'','').replace('[','
 
 
 conf_min_x = '-113107.81'
-conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
 conf_min_y = '214783.87'
 conf_max_y = '726783.87'
 conf_max_x = '398892.19'
-conf_remote_path_retiled = pathlib.Path( '/webdav/LAZ' + '/retiled_'+param_username)
 conf_n_tiles_side = '512'
-conf_remote_path_split = pathlib.Path( '/webdav/LAZ' + '/split_'+param_username)
 conf_local_tmp = pathlib.Path('/tmp')
 
 split_laz_files
