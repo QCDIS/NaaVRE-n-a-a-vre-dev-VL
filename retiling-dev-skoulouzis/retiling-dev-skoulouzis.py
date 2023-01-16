33cdf6b7a73e866198da082a11e2f016f56f17bc
skoulouzis <skoulouzis@users.noreply.github.com>
2023-01-16 20:39:09 +0100

    retiling-dev-skoulouzis update

--- retiling-dev-skoulouzis/retiling-dev-skoulouzis.py
+++ retiling-dev-skoulouzis/retiling-dev-skoulouzis.py
@@ -27,25 +27,25 @@ param_login = args.param_login
 param_password = args.param_password
 param_username = args.param_username
 
+conf_remote_path_split = pathlib.Path( '/webdav/LAZ' + '/split_'+param_username)
 conf_min_x = '-113107.81'
-conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
-conf_min_y = '214783.87'
-conf_max_y = '726783.87'
-conf_max_x = '398892.19'
 conf_remote_path_retiled = pathlib.Path( '/webdav/LAZ' + '/retiled_'+param_username)
 conf_n_tiles_side = '512'
-conf_remote_path_split = pathlib.Path( '/webdav/LAZ' + '/split_'+param_username)
+conf_max_x = '398892.19'
+conf_max_y = '726783.87'
 conf_local_tmp = pathlib.Path('/tmp')
+conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
+conf_min_y = '214783.87'
 
+conf_remote_path_split = pathlib.Path( '/webdav/LAZ' + '/split_'+param_username)
 conf_min_x = '-113107.81'
-conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
-conf_min_y = '214783.87'
-conf_max_y = '726783.87'
-conf_max_x = '398892.19'
 conf_remote_path_retiled = pathlib.Path( '/webdav/LAZ' + '/retiled_'+param_username)
 conf_n_tiles_side = '512'
-conf_remote_path_split = pathlib.Path( '/webdav/LAZ' + '/split_'+param_username)
+conf_max_x = '398892.19'
+conf_max_y = '726783.87'
 conf_local_tmp = pathlib.Path('/tmp')
+conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
+conf_min_y = '214783.87'
 split_laz_files
 remote_path_retiled = str(conf_remote_path_retiled)
 
