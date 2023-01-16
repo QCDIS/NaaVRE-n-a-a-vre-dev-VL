e44d077b0a450d00779131607f8402fde11ffe17
skoulouzis <skoulouzis@users.noreply.github.com>
2023-01-16 23:01:08 +0100

    retiling-dev-skoulouzis update

--- retiling-dev-skoulouzis/retiling-dev-skoulouzis.py
+++ retiling-dev-skoulouzis/retiling-dev-skoulouzis.py
@@ -1,4 +1,3 @@
-from laserfarm import Retiler
 import pathlib
 
 import argparse
@@ -9,10 +8,6 @@ arg_parser.add_argument('--id', action='store', type=str, required=True, dest='i
 
 arg_parser.add_argument('--split_laz_files', action='store', type=str, required='True', dest='split_laz_files')
 
-arg_parser.add_argument('--param_hostname', action='store', type=str, required='True', dest='param_hostname')
-arg_parser.add_argument('--param_login', action='store', type=str, required='True', dest='param_login')
-arg_parser.add_argument('--param_password', action='store', type=str, required='True', dest='param_password')
-arg_parser.add_argument('--param_username', action='store', type=str, required='True', dest='param_username')
 
 args = arg_parser.parse_args()
 print(args)
@@ -22,54 +17,19 @@ id = args.id
 import json
 split_laz_files = json.loads(args.split_laz_files.replace('\'','').replace('[','["').replace(']','"]'))
 
-param_hostname = args.param_hostname
-param_login = args.param_login
-param_password = args.param_password
-param_username = args.param_username
 
-conf_remote_path_split = pathlib.Path( '/webdav/LAZ' + '/split_'+param_username)
-conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
-conf_remote_path_retiled = pathlib.Path( '/webdav/LAZ' + '/retiled_'+param_username)
-conf_max_y = '726783.87'
-conf_min_y = '214783.87'
 conf_min_x = '-113107.81'
+conf_min_y = '214783.87'
+conf_max_y = '726783.87'
 conf_max_x = '398892.19'
-conf_local_tmp = pathlib.Path('/tmp')
 conf_n_tiles_side = '512'
+conf_local_tmp = pathlib.Path('/tmp')
 
-conf_remote_path_split = pathlib.Path( '/webdav/LAZ' + '/split_'+param_username)
-conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
-conf_remote_path_retiled = pathlib.Path( '/webdav/LAZ' + '/retiled_'+param_username)
-conf_max_y = '726783.87'
-conf_min_y = '214783.87'
-conf_min_x = '-113107.81'
-conf_max_x = '398892.19'
-conf_local_tmp = pathlib.Path('/tmp')
-conf_n_tiles_side = '512'
 split_laz_files
-remote_path_retiled = str(conf_remote_path_retiled)
-
-grid_retile = {
-    'min_x': float(conf_min_x),
-    'max_x': float(conf_max_x),
-    'min_y': float(conf_min_y),
-    'max_y': float(conf_max_y),
-    'n_tiles_side': int(conf_n_tiles_side)
-}
-
-retiling_input = {
-    'setup_local_fs': {'tmp_folder': conf_local_tmp.as_posix()},
-    'pullremote': conf_remote_path_split.as_posix(),
-    'set_grid': grid_retile,
-    'split_and_redistribute': {},
-    'validate': {},
-    'pushremote': conf_remote_path_retiled.as_posix(),
-    'cleanlocalfs': {}
-}
 
 for file in split_laz_files:
-    retiler = Retiler(file.replace('"',''),label=file).config(retiling_input).setup_webdav_client(conf_wd_opts)
-    retiler_output = retiler.run()
+    print(file)
+    retiler_output = file
 
 import json
 filename = "/tmp/retiler_output_" + id + ".json"
