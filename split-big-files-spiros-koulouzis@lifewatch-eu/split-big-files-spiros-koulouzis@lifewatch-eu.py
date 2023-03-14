4fcecedef4a4c184bfde3e4440ba984d1432303d
skoulouzis <skoulouzis@users.noreply.github.com>
2023-03-14 12:31:12 +0100

    split-big-files-spiros-koulouzis@lifewatch-eu update

--- split-big-files-spiros-koulouzis@lifewatch-eu/split-big-files-spiros-koulouzis@lifewatch-eu.py
+++ split-big-files-spiros-koulouzis@lifewatch-eu/split-big-files-spiros-koulouzis@lifewatch-eu.py
@@ -1,8 +1,8 @@
-import numpy as np
-import pathlib
 import os
-from webdav3.client import Client
 import laspy
+import pathlib
+from webdav3.client import Client
+import numpy as np
 
 import argparse
 arg_parser = argparse.ArgumentParser()
@@ -15,6 +15,7 @@ arg_parser.add_argument('--laz_files', action='store', type=str, required='True'
 arg_parser.add_argument('--param_hostname', action='store', type=str, required='True', dest='param_hostname')
 arg_parser.add_argument('--param_login', action='store', type=str, required='True', dest='param_login')
 arg_parser.add_argument('--param_password', action='store', type=str, required='True', dest='param_password')
+arg_parser.add_argument('--param_username', action='store', type=str, required='True', dest='param_username')
 
 args = arg_parser.parse_args()
 print(args)
@@ -27,20 +28,20 @@ laz_files = json.loads(args.laz_files.replace('\'','').replace('[','["').replace
 param_hostname = args.param_hostname
 param_login = args.param_login
 param_password = args.param_password
+param_username = args.param_username
 
-conf_remote_path_retiled = pathlib.Path( '/webdav/LAZ' + '/retiled_'+ 'myname')
-conf_remote_path_ahn =  '/webdav/LAZ'
 conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
-conf_remote_path_split = pathlib.Path( '/webdav/LAZ' + '/split_'+ 'myname')
-conf_max_filesize = '262144000'  # desired max file size (in bytes)
 conf_laz_compression_factor = '7'
+conf_max_filesize = '262144000'  # desired max file size (in bytes)
+conf_remote_path_split = pathlib.Path( '/webdav/LAZ' + '/split_'+param_username)
+conf_remote_path_ahn =  '/webdav/LAZ'
 
-conf_remote_path_retiled = pathlib.Path( '/webdav/LAZ' + '/retiled_'+ 'myname')
-conf_remote_path_ahn =  '/webdav/LAZ'
 conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
-conf_remote_path_split = pathlib.Path( '/webdav/LAZ' + '/split_'+ 'myname')
-conf_max_filesize = '262144000'  # desired max file size (in bytes)
 conf_laz_compression_factor = '7'
+conf_max_filesize = '262144000'  # desired max file size (in bytes)
+conf_remote_path_split = pathlib.Path( '/webdav/LAZ' + '/split_'+param_username)
+conf_remote_path_ahn =  '/webdav/LAZ'
+
 
 def save_chunk_to_laz_file(in_filename, 
                            out_filename, 
@@ -83,8 +84,9 @@ client.mkdir(conf_remote_path_split.as_posix())
 
 remote_path_split = conf_remote_path_split
 
+
 for file in laz_files:
-    print('Splitting: '+file)
+    print('Splitting: '+file )
     client.download_sync(remote_path=os.path.join(conf_remote_path_ahn,file), local_path=file)
     inps = split_strategy(file, int(conf_max_filesize))
     for inp in inps:
@@ -95,11 +97,11 @@ for file in laz_files:
         if not f.endswith('.LAZ'):
             continue
         os.remove(os.path.join('.', f))
-        
-remote_path_retiled = str(conf_remote_path_retiled)
+    
+split_laz_files = laz_files
 
 import json
-filename = "/tmp/remote_path_retiled_" + id + ".json"
-file_remote_path_retiled = open(filename, "w")
-file_remote_path_retiled.write(json.dumps(remote_path_retiled))
-file_remote_path_retiled.close()
+filename = "/tmp/split_laz_files_" + id + ".json"
+file_split_laz_files = open(filename, "w")
+file_split_laz_files.write(json.dumps(split_laz_files))
+file_split_laz_files.close()
