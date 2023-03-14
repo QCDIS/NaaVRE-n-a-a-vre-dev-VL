5f8a11e275313e564e0407c55471c7b75a814efb
skoulouzis <skoulouzis@users.noreply.github.com>
2023-03-14 12:30:00 +0100

    setup-clean-working-directory-joristimmermans creation

---
+++ setup-clean-working-directory-joristimmermans/setup-clean-working-directory-joristimmermans.py
@@ -0,0 +1,39 @@
+import os
+import shutil
+
+import argparse
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--working_directory_name', action='store', type=str, required='True', dest='working_directory_name')
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+working_directory_name = args.working_directory_name
+
+
+
+def get_working_dir(dir_name: str) -> str:
+    working_dir = f'/datastore/working_dirs/{dir_name}'
+    working_dir = f'/home/jovyan/data/working_dirs/{dir_name}'
+    if os.path.exists(working_dir):
+        shutil.rmtree(working_dir)
+    os.makedirs(working_dir)
+    return working_dir
+
+name = working_directory_name
+working_dir = get_working_dir(name)
+
+print(working_dir)
+
+import json
+filename = "/tmp/working_dir_" + id + ".json"
+file_working_dir = open(filename, "w")
+file_working_dir.write(json.dumps(working_dir))
+file_working_dir.close()
