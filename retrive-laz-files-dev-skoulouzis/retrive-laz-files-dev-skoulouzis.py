aab023277dce99287cda8bcb7098d572f9db4381
skoulouzis <skoulouzis@users.noreply.github.com>
2023-03-31 15:34:33 +0200

    retrive-laz-files-dev-skoulouzis creation

---
+++ retrive-laz-files-dev-skoulouzis/retrive-laz-files-dev-skoulouzis.py
@@ -0,0 +1,36 @@
+from laserfarm.remote_utils import get_wdclient
+from laserfarm.remote_utils import list_remote
+import pathlib
+
+import argparse
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--param_hostname', action='store', type=str, required='True', dest='param_hostname')
+arg_parser.add_argument('--param_login', action='store', type=str, required='True', dest='param_login')
+arg_parser.add_argument('--param_password', action='store', type=str, required='True', dest='param_password')
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+
+param_hostname = args.param_hostname
+param_login = args.param_login
+param_password = args.param_password
+
+conf_remote_path_ahn =  '/webdav/LAZ'
+
+conf_remote_path_ahn =  '/webdav/LAZ'
+conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
+laz_files = [f for f in list_remote(get_wdclient(conf_wd_opts), pathlib.Path(conf_remote_path_ahn).as_posix())
+             if f.lower().endswith('.laz')]
+
+import json
+filename = "/tmp/laz_files_" + id + ".json"
+file_laz_files = open(filename, "w")
+file_laz_files.write(json.dumps(laz_files))
+file_laz_files.close()
