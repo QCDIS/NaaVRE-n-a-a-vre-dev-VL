0331e129ec3f2ac45a798763da20beaebb94afab
skoulouzis <skoulouzis@users.noreply.github.com>
2023-04-25 12:45:48 +0300

    untitled-diederikblanson creation

---
+++ untitled-diederikblanson/untitled-diederikblanson.py
@@ -0,0 +1,24 @@
+
+import argparse
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--a', action='store', type=int, required='True', dest='a')
+
+arg_parser.add_argument('--b', action='store', type=int, required='True', dest='b')
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+a = args.a
+b = args.b
+
+
+
+c = a * b
+
