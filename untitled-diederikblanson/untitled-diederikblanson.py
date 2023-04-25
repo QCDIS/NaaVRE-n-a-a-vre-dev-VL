d8e522a49e69e3280d54532db2c654d6242b1719
skoulouzis <skoulouzis@users.noreply.github.com>
2023-04-25 14:16:16 +0300

    untitled-diederikblanson update

--- untitled-diederikblanson/untitled-diederikblanson.py
+++ untitled-diederikblanson/untitled-diederikblanson.py
@@ -5,20 +5,16 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
 
-arg_parser.add_argument('--a', action='store', type=int, required='True', dest='a')
-
-arg_parser.add_argument('--b', action='store', type=int, required='True', dest='b')
-
 
 args = arg_parser.parse_args()
 print(args)
 
 id = args.id
 
-a = args.a
-b = args.b
 
 
 
-c = a * b
+a = 3 
+b = a * c
+b
 
