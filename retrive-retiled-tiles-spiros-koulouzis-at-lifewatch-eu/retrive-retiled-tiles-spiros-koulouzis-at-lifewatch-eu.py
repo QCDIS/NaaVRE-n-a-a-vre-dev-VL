39089fdc42f8edf003643fa86f0a2cd17baeb162
skoulouzis <skoulouzis@users.noreply.github.com>
2023-05-02 17:18:37 +0200

    retrive-retiled-tiles-spiros-koulouzis-at-lifewatch-eu update

--- retrive-retiled-tiles-spiros-koulouzis-at-lifewatch-eu/retrive-retiled-tiles-spiros-koulouzis-at-lifewatch-eu.py
+++ retrive-retiled-tiles-spiros-koulouzis-at-lifewatch-eu/retrive-retiled-tiles-spiros-koulouzis-at-lifewatch-eu.py
@@ -1,6 +1,6 @@
-import fnmatch
-from laserfarm.remote_utils import list_remote
 import pathlib
+from laserfarm.remote_utils import list_remote
+import fnmatch
 from laserfarm.remote_utils import get_wdclient
 
 import argparse
@@ -14,7 +14,6 @@ arg_parser.add_argument('--remote_path_retiled', action='store', type=str, requi
 arg_parser.add_argument('--param_hostname', action='store', type=str, required='True', dest='param_hostname')
 arg_parser.add_argument('--param_login', action='store', type=str, required='True', dest='param_login')
 arg_parser.add_argument('--param_password', action='store', type=str, required='True', dest='param_password')
-arg_parser.add_argument('--param_username', action='store', type=str, required='True', dest='param_username')
 
 args = arg_parser.parse_args()
 print(args)
@@ -26,13 +25,12 @@ remote_path_retiled = args.remote_path_retiled
 param_hostname = args.param_hostname
 param_login = args.param_login
 param_password = args.param_password
-param_username = args.param_username
 
-conf_remote_path_retiled = pathlib.Path( '/webdav/LAZ' + '/retiled_'+param_username)
 conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
+conf_remote_path_retiled = pathlib.Path( '/webdav/LAZ' + '/retiled_'+ 'myname')
 
-conf_remote_path_retiled = pathlib.Path( '/webdav/LAZ' + '/retiled_'+param_username)
 conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
+conf_remote_path_retiled = pathlib.Path( '/webdav/LAZ' + '/retiled_'+ 'myname')
 remote_path_retiled
 
 tiles = [t.strip('/') for t in list_remote(get_wdclient(conf_wd_opts), conf_remote_path_retiled.as_posix())
