a5473156bdd962f8f66bbc82d5ed654150bc31e2
skoulouzis <skoulouzis@users.noreply.github.com>
2023-05-06 18:17:36 +0200

    untitled-diederikblanson update

--- untitled-diederikblanson/untitled-diederikblanson.py
+++ untitled-diederikblanson/untitled-diederikblanson.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 import argparse
 arg_parser = argparse.ArgumentParser()
@@ -5,16 +6,27 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
 
+arg_parser.add_argument('--b', action='store', type=int, required='True', dest='b')
+
+arg_parser.add_argument('--foo', action='store', type=int, required='True', dest='foo')
+
 
 args = arg_parser.parse_args()
 print(args)
 
 id = args.id
 
+b = args.b
+foo = args.foo
 
 
 
-a = 3 
-b = a * c
-b
+np.array 
 
+a = b + 1 + foo()
+
+import json
+filename = "/tmp/a_" + id + ".json"
+file_a = open(filename, "w")
+file_a.write(json.dumps(a))
+file_a.close()
