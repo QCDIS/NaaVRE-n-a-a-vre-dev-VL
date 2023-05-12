288118e2d92cc3f165b56ea5ad2b9d8bfd0c16ad
skoulouzis <skoulouzis@users.noreply.github.com>
2023-05-12 20:09:06 +0200

    test-diederik-r2-diederik creation

---
+++ test-diederik-r2-diederik/test-diederik-r2-diederik.R
@@ -0,0 +1,30 @@
+setwd('/app') 
+
+# retrieve input parameters
+library(optparse) 
+option_list = list( 
+	 make_option(c("--a"), action="store", default=NA, type='integer', help="my description"),
+	 make_option(c("--id"), action="store", default=NA, type='character', help="my description")
+)
+
+# set input parameters accordingly 
+opt = parse_args(OptionParser(option_list=option_list)) 
+library(jsonlite) 
+a = opt$a 
+id = opt$id 
+
+# check if the fields are set 
+if(is.na(a)){ 
+   stop('the `a` parameter is not correctly set. See script usage (--help)') 
+}
+if(is.na(id)){ 
+   stop('the `id` parameter is not correctly set. See script usage (--help)') 
+}
+
+# source code 
+b <- 5 * a * 12
+
+# capturing outputs 
+file <- file(paste0('/tmp/b_', id, '.json')) 
+writeLines(toJSON(b, auto_unbox=TRUE), file) 
+close(file) 
