de70ef2f4f1954c5e745f744eb66d3e4b53239fa
skoulouzis <skoulouzis@users.noreply.github.com>
2023-05-12 18:20:26 +0200

    test-diederik-python-diederik update

--- test-diederik-python-diederik/test-diederik-python-diederik.py
+++ test-diederik-python-diederik/test-diederik-python-diederik.py
@@ -5,19 +5,18 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
 
-arg_parser.add_argument('--a', action='store', type=int, required='True', dest='a')
-
+arg_parser.add_argument('--param_a', action='store', type=int, required='True', dest='param_a')
 
 args = arg_parser.parse_args()
 print(args)
 
 id = args.id
 
-a = args.a
+
+param_a = args.param_a
 
 
-
-b = 5 * a * 12
+b = 5 * param_a * 12
 
 import json
 filename = "/tmp/b_" + id + ".json"
