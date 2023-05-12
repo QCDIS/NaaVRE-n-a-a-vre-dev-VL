fed0b966cf19972dea1eb94409631c359a53c931
skoulouzis <skoulouzis@users.noreply.github.com>
2023-05-12 15:25:26 +0200

    test-diederik-today3-diederik update

--- test-diederik-today3-diederik/test-diederik-today3-diederik.R
+++ test-diederik-today3-diederik/test-diederik-today3-diederik.R
@@ -3,22 +3,19 @@ setwd('/app')
 # retrieve input parameters
 library(optparse) 
 option_list = list( 
-	 make_option(c("--b"), action="store", default=NA, type='integer', help="my description")
+	 make_option(c("--b"), action="store", default=NA, type='integer', help="my description"),
+	 make_option(c("--id"), action="store", default=NA, type='character', help="my description")
 )
 
 # set input parameters accordingly 
 opt = parse_args(OptionParser(option_list=option_list)) 
 library(jsonlite) 
 b = fromJSON(opt$b) 
-a = fromJSON(opt$a) 
 id = fromJSON(opt$id) 
 
 # check if the fields are set 
 if(is.na(b){ 
    stop('the `b` parameter is not correctly set. See script usage (--help)') 
-}
-if(is.na(a){ 
-   stop('the `a` parameter is not correctly set. See script usage (--help)') 
 }
 if(is.na(id){ 
    stop('the `id` parameter is not correctly set. See script usage (--help)') 
