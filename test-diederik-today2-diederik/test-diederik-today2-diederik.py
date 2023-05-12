9c7ca11918d6929d8a39a720bdb18a4f7550f6a5
skoulouzis <skoulouzis@users.noreply.github.com>
2023-05-12 17:53:53 +0200

    test-diederik-today2-diederik update

--- test-diederik-today2-diederik/test-diederik-today2-diederik.py
+++ test-diederik-today2-diederik/test-diederik-today2-diederik.py
@@ -5,16 +5,19 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
 
+arg_parser.add_argument('--b', action='store', type=int, required='True', dest='b')
+
 
 args = arg_parser.parse_args()
 print(args)
 
 id = args.id
 
+b = args.b
 
 
 
-a = 3
+a = 3 * b
 
 import json
 filename = "/tmp/a_" + id + ".json"
