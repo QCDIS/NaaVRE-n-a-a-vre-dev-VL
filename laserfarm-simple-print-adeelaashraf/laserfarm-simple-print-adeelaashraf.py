1ff770a025ec9e2e99f7aa1f7f22204ae5dcf8d6
skoulouzis <skoulouzis@users.noreply.github.com>
2023-06-08 12:34:13 +0200

    laserfarm-simple-print-adeelaashraf creation

---
+++ laserfarm-simple-print-adeelaashraf/laserfarm-simple-print-adeelaashraf.py
@@ -0,0 +1,20 @@
+
+import argparse
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--param_a', action='store', type=list, required='True', dest='param_a')
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+
+param_a = args.param_a
+
+
+print(param_a)
+
