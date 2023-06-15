67f66e19357f2b794b56572ee5452bb49af330d7
skoulouzis <skoulouzis@users.noreply.github.com>
2023-06-15 12:58:39 +0200

    start-1-marylm88 creation

---
+++ start-1-marylm88/start-1-marylm88.R
@@ -0,0 +1,100 @@
+setwd('/app')
+
+# retrieve input parameters
+
+library(optparse)
+library(jsonlite)
+
+option_list = list
+
+option_list = list(
+make_option(c("--id"), action="store", default=NA, type='character', help="my description")
+
+
+)
+
+# set input parameters accordingly
+opt = parse_args(OptionParser(option_list=option_list))
+
+
+id = opt$id
+
+
+
+
+
+
+
+
+df.datain=read.csv(param_datain,stringsAsFactors=FALSE,sep = ";", dec = ".")
+df.datain[,'measurementremarks'] = tolower(df.datain[,'measurementremarks']) # eliminate capital letters
+df.datain[,'index'] = c(1:nrow(df.datain)) # needed to restore rows order later
+
+df.operator=read.csv('input/2_FILEinformativo_OPERATORE.csv',stringsAsFactors=FALSE,sep = ";", dec = ".") # load internal database 
+df.operator[df.operator==('no')]<-NA
+df.operator[df.operator==('see note')]<-NA
+
+df.merged = merge(x = df.datain, y = df.operator, by = c("scientificname","measurementremarks"), all.x = TRUE)
+
+if(!'diameterofsedimentationchamber'%in%names(df.merged))df.merged[,'diameterofsedimentationchamber']=NA
+if(!'diameteroffieldofview'%in%names(df.merged))df.merged[,'diameteroffieldofview']=NA
+if(!'transectcounting'%in%names(df.merged))df.merged[,'transectcounting']=NA
+if(!'numberofcountedfields'%in%names(df.merged))df.merged[,'numberofcountedfields']=df.merged[,'transectcounting']
+if(!'numberoftransects'%in%names(df.merged))df.merged[,'numberoftransects']=df.merged[,'transectcounting']
+if(!'settlingvolume'%in%names(df.merged))df.merged[,'settlingvolume']=NA
+if(!'dilutionfactor'%in%names(df.merged))df.merged[,'dilutionfactor']=1
+
+
+
+formulaformissingdimension = '' 
+formulaformissingdimensionsimplified = '' 
+df.temp = ''  
+md.form = ''
+df.temp2 = ''
+index = ''
+md = ''
+df.merged.concat = '' 
+md.formulas = ''
+
+
+if(param_CalcType=='advanced'){
+  df.merged.concat = df.merged[is.na(df.merged[,'formulaformissingdimension']),]
+  md.formulas = unique(df.merged[!is.na(df.merged[,'formulaformissingdimension']),'formulaformissingdimension'])
+  for(md.form in md.formulas){
+    df.temp = subset(df.merged,formulaformissingdimension==md.form)
+    for(md in unique(df.temp[,'missingdimension'])){
+      df.temp2 = df.temp[df.temp[,'missingdimension']==md,]
+      df.temp2[[md]] = round(with(df.temp2,eval(parse(text=md.form))),2)
+      df.merged.concat = rbind(df.merged.concat,df.temp2)
+    }
+  }
+  df.merged.concat = df.merged.concat[order(df.merged.concat[,'index']),]
+  df.merged = df.merged.concat
+} else if(param_CalcType=='simplified'){
+  df.merged.concat = df.merged[is.na(df.merged[,'formulaformissingdimensionsimplified']),]
+  md.formulas = unique(df.merged[!is.na(df.merged[,'formulaformissingdimensionsimplified']),'formulaformissingdimensionsimplified'])
+  for(md.form in md.formulas){
+    df.temp = subset(df.merged,formulaformissingdimensionsimplified==md.form)
+    for(md in unique(df.temp[,'missingdimensionsimplified'])){
+      df.temp2 = df.temp[df.temp[,'missingdimensionsimplified']==md,]
+      df.temp2[[md]] = round(with(df.temp2,eval(parse(text=md.form))),2)
+      df.merged.concat = rbind(df.merged.concat,df.temp2)
+    }
+  }
+  df.merged.concat = df.merged.concat[order(df.merged.concat[,'index']),]
+  df.merged = df.merged.concat
+}
+    
+
+output_dfmerged = 'output/dfmerged.csv'
+write.table(df.merged,paste(output_dfmerged,sep=''),row.names=FALSE,sep = ";",dec = ".",quote=FALSE)
+
+
+
+# capturing outputs
+file <- file(paste0('/tmp/output_dfmerged_', id, '.json'))
+writeLines(toJSON(output_dfmerged, auto_unbox=TRUE), file)
+close(file)
+file <- file(paste0('/tmp/df.merged_', id, '.json'))
+writeLines(toJSON(df.merged, auto_unbox=TRUE), file)
+close(file)
