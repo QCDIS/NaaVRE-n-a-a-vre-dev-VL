fdd5d4e1a13f0a303a9ec6633657e393c8bf2cb3
skoulouzis <skoulouzis@users.noreply.github.com>
2023-06-26 11:25:05 +0200

    26june-test-diederikblanson creation

---
+++ 26june-test-diederikblanson/26june-test-diederikblanson.py
@@ -0,0 +1,29 @@
+
+import argparse
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--a', action='store', type=int, required='True', dest='a')
+
+arg_parser.add_argument('--b', action='store', type=int, required='True', dest='b')
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+a = args.a
+b = args.b
+
+
+
+c = a * b
+
+import json
+filename = "/tmp/c_" + id + ".json"
+file_c = open(filename, "w")
+file_c.write(json.dumps(c))
+file_c.close()
