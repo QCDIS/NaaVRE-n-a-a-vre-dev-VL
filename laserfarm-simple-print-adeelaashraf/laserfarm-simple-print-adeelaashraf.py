3bb9fa823fd0c26c30460ebbaa45fc484eae8e85
skoulouzis <skoulouzis@users.noreply.github.com>
2023-07-10 21:25:03 +0200

    laserfarm-simple-print-adeelaashraf update

--- laserfarm-simple-print-adeelaashraf/laserfarm-simple-print-adeelaashraf.py
+++ laserfarm-simple-print-adeelaashraf/laserfarm-simple-print-adeelaashraf.py
@@ -5,7 +5,7 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
 
-arg_parser.add_argument('--param_a', action='store', type=list, required='True', dest='param_a')
+arg_parser.add_argument('--param_a', action='store', type=str, required='True', dest='param_a')
 
 args = arg_parser.parse_args()
 print(args)
