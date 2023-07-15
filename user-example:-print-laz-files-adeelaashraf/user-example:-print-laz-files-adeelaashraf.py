ef8660c2e78b9ab85d8c59a454009fbbb5b5515c
skoulouzis <skoulouzis@users.noreply.github.com>
2023-07-15 22:06:49 +0200

    user-example:-print-laz-files-adeelaashraf creation

---
+++ user-example:-print-laz-files-adeelaashraf/user-example:-print-laz-files-adeelaashraf.py
@@ -0,0 +1,22 @@
+
+import argparse
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--laz_files', action='store', type=str, required='True', dest='laz_files')
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+import json
+laz_files = json.loads(args.laz_files.replace('\'','').replace('[','["').replace(']','"]'))
+
+
+
+print(laz_files)
+
