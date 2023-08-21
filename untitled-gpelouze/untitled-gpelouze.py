c91303cb2b1ac99b168a5acdd4cac2308e1a2c50
skoulouzis <skoulouzis@users.noreply.github.com>
2023-08-21 14:38:41 +0300

    untitled-gpelouze update

--- untitled-gpelouze/untitled-gpelouze.py
+++ untitled-gpelouze/untitled-gpelouze.py
@@ -5,19 +5,17 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
 
+arg_parser.add_argument('--a', action='store', type=int, required='True', dest='a')
+
 
 args = arg_parser.parse_args()
 print(args)
 
 id = args.id
 
+a = args.a
 
 
 
-a = 1
+print(a)
 
-import json
-filename = "/tmp/a_" + id + ".json"
-file_a = open(filename, "w")
-file_a.write(json.dumps(a))
-file_a.close()
