b67a485b7d24d9ebc9b55888fcea0a35c9df059d
skoulouzis <skoulouzis@users.noreply.github.com>
2023-08-28 10:39:28 +0300

    print-gpelouze update

--- print-gpelouze/print-gpelouze.py
+++ print-gpelouze/print-gpelouze.py
@@ -5,7 +5,7 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
 
-arg_parser.add_argument('--my_var', action='store', type=int, required='True', dest='my_var')
+arg_parser.add_argument('--a', action='store', type=int, required='True', dest='a')
 
 
 args = arg_parser.parse_args()
@@ -13,9 +13,9 @@ print(args)
 
 id = args.id
 
-my_var = args.my_var
+a = args.a
 
 
 
-print(my_var)
+print(a)
 
