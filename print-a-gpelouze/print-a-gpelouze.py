02bb2c5064cd2448fb9a694ab2b8b7822183085a
skoulouzis <skoulouzis@users.noreply.github.com>
2023-09-22 18:08:36 +0200

    print-a-gpelouze creation

---
+++ print-a-gpelouze/print-a-gpelouze.py
@@ -0,0 +1,21 @@
+
+import argparse
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--a', action='store', type=int, required='True', dest='a')
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+a = args.a
+
+
+
+print(a)
+
